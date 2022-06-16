e5463571cf734415e4c394facf0cbadc0c31274a
wim-web <45655999+wim-web@users.noreply.github.com>
2022-06-17 00:46:37 +0900

    install.sh for dotfiles

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+script_dir=$(cd $(dirname $0); pwd)
+
+mkdir -p ~/.config/fish
+ln -s $script_dir/ansible/roles/fish/files/conf.d/* ~/.config/fish/conf.d/
+ln -s $script_dir/ansible/roles/fish/files/functions/* ~/.config/fish/functions/
+
+ln -s $script_dir/ansible/roles/git/files/.gitconfig ~/.gitconfig
+
