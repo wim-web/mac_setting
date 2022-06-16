9a707661bc38d77226769f70a0cdf1ed074c32f0
wim-web <45655999+wim-web@users.noreply.github.com>
2022-06-17 01:55:08 +0900

    global gitignore

--- install.sh
+++ install.sh
@@ -6,5 +6,7 @@ mkdir -p ~/.config/fish
 ln -s $script_dir/ansible/roles/fish/files/conf.d/* ~/.config/fish/conf.d/
 ln -s $script_dir/ansible/roles/fish/files/functions/* ~/.config/fish/functions/
 
+mkdir -p ~/.config/git
 ln -s $script_dir/ansible/roles/git/files/.gitconfig ~/.gitconfig
+ln -s $script_dir/ansible/roles/git/files/ignore ~/.config/git/ignore
 
