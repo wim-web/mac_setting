dda23951d238de24b8eabd6a6c33a3cd8f750c4f
wim-web <45655999+wim-web@users.noreply.github.com>
2022-06-18 23:07:18 +0900

    install fish

--- install.sh
+++ install.sh
@@ -2,11 +2,29 @@
 
 script_dir=$(cd $(dirname $0); pwd)
 
-mkdir -p ~/.config/fish
-ln -s $script_dir/ansible/roles/fish/files/conf.d/* ~/.config/fish/conf.d/
-ln -s $script_dir/ansible/roles/fish/files/functions/* ~/.config/fish/functions/
+function fish_install() {
+    sudo apt-get update
+    sudo apt-get -y install --no-install-recommends fish
 
-mkdir -p ~/.config/git
-ln -s $script_dir/ansible/roles/git/files/.gitconfig ~/.gitconfig
-ln -s $script_dir/ansible/roles/git/files/ignore ~/.config/git/ignore
+    fish -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && echo jorgebucaran/fisher | fisher install"
+    fish -c "echo oh-my-fish/theme-bobthefish | fisher install "
+}
+
+function fish_config() {
+    mkdir -p ~/.config/fish
+    ln -s $script_dir/ansible/roles/fish/files/conf.d/* ~/.config/fish/conf.d/
+    ln -s $script_dir/ansible/roles/fish/files/functions/* ~/.config/fish/functions/
+}
+
+function git_config() {
+    mkdir -p ~/.config/git
+    ln -s $script_dir/ansible/roles/git/files/.gitconfig ~/.gitconfig
+    ln -s $script_dir/ansible/roles/git/files/ignore ~/.config/git/ignore
+}
+
+fish_install
+fish_config
+git_config
+
+
 
