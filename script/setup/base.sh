68121a2b9ad419a684bc330c3fbcc580ac031583
wim-web <45655999+wim-web@users.noreply.github.com>
2023-06-18 22:13:58 +0900

    add brew path

--- script/setup/base.sh
+++ script/setup/base.sh
@@ -7,6 +7,7 @@ if type brew >/dev/null; then
     echo "Already installed homebrew"
 else
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    PATH=$PATH:/opt/homebrew/bin
 fi
 
 # fish shell
